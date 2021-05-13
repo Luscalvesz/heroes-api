f1b5a38af26b019c398dc04ea1e81221a2decbeb
lucas alves <lucas.pereira@codeby.com.br>
2021-05-12 23:31:39 -0300

    adding style

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,14 +7,14 @@ const favTrue = '../assets/images/red_star.png';
 
 
 async function searchHero(){
-  let nameHeroe = document.getElementById('name').value
+  let nameHero = document.getElementById('name').value
   let resp = document.getElementById('resp')
   const options = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json', Accept: 'application/vnd.vtex.ds.v10+json' }
   };
   resp.innerHTML = ""
-  fetch(`https://codeby-cors.integrationby.com.br/https://superheroapi.com/api/3713047672155764/search/${nameHeroe}`, options)
+  fetch(`https://codeby-cors.integrationby.com.br/https://superheroapi.com/api/3713047672155764/search/${nameHero}`, options)
   // fetch(`https://codeby-cors.integrationby.com.br/https://superheroapi.com/api/3713047672155764/${number}`, options)
     .then(response => response.text())
     .then(result => {
@@ -22,38 +22,39 @@ async function searchHero(){
       data.results.forEach(element => {
         console.log(element);
         console.log(element.name);
-
-
-
-
-        // resp.innerHTML += `<p class="item">Name: ${element.name}</p>`
+        
         resp.innerHTML += `
-        <div class="heroe">
-          <img class="heroe-img" src="${element.image.url}">
-          <p class="heroe-name">Name: ${element.name}</p>
-          <div class="heroe-stats">
-            <h3 class="heroe-stats-title">Stats</h3>
-            <p class="heroe-stats-value">Combat: ${element.powerstats.combat}</p>
-            <p class="heroe-stats-value">Durability: ${element.powerstats.durability}</p>
-            <p class="heroe-stats-value">Intelligence: ${element.powerstats.intelligence}</p>
-            <p class="heroe-stats-value">Power: ${element.powerstats.power}</p>
-            <p class="heroe-stats-value">Speed: ${element.powerstats.speed}</p>
-            <p class="heroe-stats-value">Strength: ${element.powerstats.strength}</p>
+        <div class="hero">
+          <img class="hero-img" src="${element.image.url}">
+          <p class="hero-name">${element.name}</p>
+          <div class="hero-stats">
+            <h3 class="hero-stats-title">Stats</h3>
+            <div class="hero-stats-box">
+              <p class="hero-stats-box-value">Combat: ${element.powerstats.combat}</p>
+              <p class="hero-stats-box-value">Durability: ${element.powerstats.durability}</p>
+            </div>
+            <div class="hero-stats-box">
+              <p class="hero-stats-box-value">Intelligence: ${element.powerstats.intelligence}</p>
+              <p class="hero-stats-box-value">Power: ${element.powerstats.power}</p>
+            </div>
+            <div class="hero-stats-box">
+              <p class="hero-stats-box-value">Speed: ${element.powerstats.speed}</p>
+              <p class="hero-stats-box-value">Strength: ${element.powerstats.strength}</p>
+            </div>
           
           </div>
         </div>
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         `
+
+
+
+        // <p class="hero-stats-value">Combat: ${element.powerstats.combat}</p>
+        //     <p class="hero-stats-value">Durability: ${element.powerstats.durability}</p>
+        //     <p class="hero-stats-value">Intelligence: ${element.powerstats.intelligence}</p>
+        //     <p class="hero-stats-value">Power: ${element.powerstats.power}</p>
+        //     <p class="hero-stats-value">Speed: ${element.powerstats.speed}</p>
+        //     <p class="hero-stats-value">Strength: ${element.powerstats.strength}</p>
 
       });
       
